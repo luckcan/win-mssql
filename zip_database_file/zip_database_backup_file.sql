8cf531e03f7c6bd0a928bdd40331d619330b5831
Andrew Lee <andrew@studyhack.tw>
2020-09-14 17:14:02 +0800

    add filename extension to 7z

--- zip_database_file/zip_database_backup_file.sql
+++ zip_database_file/zip_database_backup_file.sql
@@ -7,5 +7,5 @@ GO
 -- Zip database backup file with overwrite mode
 DECLARE @logname varchar(50), @zipbak varchar(200)
 SET @logname = 'X:\zipbak-' + CONVERT(CHAR(8), GETDATE(), 112) + '.log'
-SET @zipbak = 'X:\7ZipCmd\7z a -aoa -tzip X:\database.bak.zip X:\database.bak -sdel >> ' + @logname
+SET @zipbak = 'X:\7ZipCmd\7z.exe a -aoa -tzip X:\database.bak.zip X:\database.bak -sdel >> ' + @logname
 EXEC XP_CMDSHELL @zipbak
